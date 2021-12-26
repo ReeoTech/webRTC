9670299701bb5a4a04d6dab0925dc920f8328ae6
Tran Khanh <tran_khanh@outlook.com>
2021-12-26 16:32:30 +0700

    feat: check browser compatiable

---
+++ src/Storage.js
@@ -0,0 +1,11 @@
+var Storage = {}
+
+if (typeof AudioContext !== 'undefined') {
+    Storage.AudioContext = AudioContext;
+} else if (typeof webkitAudioContext !== 'undefined') {
+    Storage.AudioContext = webkitAudioContext;
+}
+
+if (typeof webRTC !== 'undefined') {
+    webRTC.Storage = Storage
+}
